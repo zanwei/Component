9e7f4c74feeabe1b3d952590a51ebfc8964d15f6
zanwei.guo <zanwei.guo@outlook.com>
2025-02-12 14:17:24 +0800

    fix: remove jotai store
    - Remove atoms.ts with jotai dependencies
    - Clean up store related files
    - Fix build errors

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -1,21 +1,3 @@
-import { atom } from 'jotai';
+export type PanelType = 'dropdown' | 'icon';
 
-// 定义面板类型
-export type PanelType = 'dropdown' | 'icon' | null;
-
-// 创建管理当前打开面板的 atom
-export const activePanelAtom = atom<PanelType>('dropdown');
-
-// 创建面板位置的 atom
-export const panelPositionAtom = atom({
-    x: 0,
-    y: 0,
-});
-
-// 创建一个派生的 atom 来处理面板的显示/隐藏逻辑
-export const togglePanelAtom = atom(
-    (get) => get(activePanelAtom),
-    (_, set, panel: PanelType) => {
-        set(activePanelAtom, panel);
-    }
-); 
+export const defaultPanel: PanelType = 'dropdown';
